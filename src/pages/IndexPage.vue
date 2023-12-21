3852ef0331ca46b61e2ee7af99d840b1d3380dc2
egentilucci <elia.gentilucci@gmail.com>
2023-12-21 22:56:59 +0100

    feat: add tab management to select new decks

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,24 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <QuestionsDeck :deck="deck"></QuestionsDeck>
+  <q-page class="column items-center">
+    <q-tabs v-model="tab" class="q-pa-md">
+      <q-tab v-for="tab in tabs" :key="tab" :name="tab" :label="tab" />
+    </q-tabs>
+    <QuestionsDeck :deck="deck" :key="tab"></QuestionsDeck>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import QuestionsDeck from '../components/QuestionsDeck.vue';
-import SDprinciplesDeck from '../assets/data/software_development_principles.json';
+import principles from '../assets/data/software_development_principles.json';
+import test from '../assets/data/software_development_test.json';
 
-const deck = SDprinciplesDeck;
+const decks = { principles, test };
+const tabs = Object.keys(decks);
+
+const deck = computed(() => {
+  return decks[tab.value];
+});
+
+const tab = ref(tabs[0] as keyof typeof decks);
 </script>
